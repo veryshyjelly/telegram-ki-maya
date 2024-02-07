867bc3c184251621a69ac1de2ccf6867103f91eb
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2024-02-07 19:29:20 +0530

    added shell script to run docker container

--- api/connect.go
+++ api/connect.go
@@ -10,6 +10,7 @@ import (
 func Connect(service subscription.Service) fiber.Handler {
 	return websocket.New(func(conn *websocket.Conn) {
 		apiKey := conn.Headers("API_KEY")
+		log.Println("GOT API KEY:", apiKey)
 		if apiKey != "API_KEY" {
 			conn.WriteMessage(websocket.TextMessage, []byte("invalid api key"))
 			return
