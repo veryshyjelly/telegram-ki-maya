789b529e328ba5fef435830cda2c0d0db5e10058
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2024-02-10 01:03:54 +0530

    fixed order in docker script

--- startDocker.sh
+++ startDocker.sh
@@ -1,3 +1,3 @@
-sudo docker stop telegram-ki-maya && sudo docker rm telegram-ki-maya
 sudo docker build -t telegram-ki-maya .
+sudo docker stop telegram-ki-maya && sudo docker rm telegram-ki-maya
 sudo docker run -d -p 8060:8060 --name telegram-ki-maya telegram-ki-maya
